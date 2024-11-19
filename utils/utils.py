bfe6cb8642eeaded51e68ff5dc69e51a5975559b
Alfio <alfiocardillo01@gmail.com>
2024-11-19 12:27:45 +0100

    add tfidf encoding

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,7 @@
 import sys
 import pandas as pd 
+from math import log
+from collections import Counter
 from ast import literal_eval
 
 array_cols = ['seconds_to_incident_sequence', 'vehicles_sequence', 'events_sequence',
@@ -22,3 +24,28 @@ def deep_copy(df: pd.DataFrame, additional_columns = []) -> pd.DataFrame:
             copy_df[col] = df[col].apply(list.copy)
 
     return copy_df
+
+
+def get_frequency(list):
+    element_counts = Counter(list)
+    total_elements = len(list)
+    return {element: count / total_elements for element, count in element_counts.items()}
+
+
+def calculate_tfidf(events_column, dumping_function):
+    events = []
+    count = Counter()
+    for events in events_column:
+        count.update(set(events))
+
+    number_of_documents_with_event = dict(count)
+    number_of_rows = len(events_column)
+
+    frequencies = events_column.apply(get_frequency)
+
+    tfidf = {}
+    for event in number_of_documents_with_event.keys(): 
+        idf = log(number_of_rows/ number_of_documents_with_event[event])
+        tfidf[event] = frequencies.apply(lambda x: idf * dumping_function(x.get(event, 0)))
+
+    return tfidf
